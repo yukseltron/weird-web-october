237fab58918f97e439b9f54c6f7cf0567a27edff
yukseltron <h.yuksel04@gmail.com>
2024-11-08 15:27:17 -0500

    16

--- 2024/16-social/main.js
+++ 2024/16-social/main.js
@@ -1,2 +1,18 @@
-document.querySelectorAll('.building').forEach((building, index) => {
-});
+function handleHover() {
+    const exhibits = document.querySelectorAll('.building');
+    for (let exhibit of exhibits) {
+      const list = exhibit.querySelector('p');
+      exhibit.addEventListener('mousemove', (e) => {
+          const mouseX = e.pageX;
+          const mouseY = e.pageY;
+          list.style.top = `${mouseY-275}px`;
+          list.style.left = `${mouseX-275}px`;
+          list.style.visibility = 'visible';
+      });
+      exhibit.addEventListener('mouseout', () => {
+          list.style.visibility = 'hidden';
+      });
+    }
+  }
+  
+  handleHover();
